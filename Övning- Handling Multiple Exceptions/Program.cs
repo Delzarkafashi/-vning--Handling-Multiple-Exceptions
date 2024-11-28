a800c2c52429dd958322fb0f871f07e0d578c241
Delzar <delzarkafashi@gmail.com>
2024-11-28 21:17:47 +0100

    Add project files.

---
+++ Övning- Handling Multiple Exceptions/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class Program
+{
+    static void HanteraInmatningOchArrayÅtkomst(string indata, int index)
+    {
+
+        int[] nummerArray = { 1, 2, 3 };
+
+        try
+        {
+            int konverteratNummer = int.Parse(indata);
+
+            int arrayVärde = nummerArray[index];
+
+            Console.WriteLine($"Värdet vid index {index} är: {arrayVärde}");
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Fel: Ogiltigt format på indata.");
+        }
+        catch (IndexOutOfRangeException)
+        {
+            Console.WriteLine($"Fel: Index {index} är utanför tillåtet intervall.");
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        KörTestfall();
+    }
+
+    static void KörTestfall()
+    {
+        Console.WriteLine("Testfall 1: Giltig indata");
+        HanteraInmatningOchArrayÅtkomst("2", 1); 
+
+        Console.WriteLine("\nTestfall 2: Ogiltig sträng");
+        HanteraInmatningOchArrayÅtkomst("abc", 1); 
+
+        Console.WriteLine("\nTestfall 3: Ogiltigt index");
+        HanteraInmatningOchArrayÅtkomst("2", 5); 
+    }
+}
